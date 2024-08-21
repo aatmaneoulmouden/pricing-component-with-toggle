2cde296364c3dec1bb17e54dfa0176ff2e38deac
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-08-21 22:39:01 +0100

    Create packages data

---
+++ src/data/packages.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        name: "Basic",
+        pricing: {
+            monthly: 19.99,
+            annually: 199.99,
+        },
+        features: [
+            "500 BG Storage",
+            "2 Users Allowed",
+            "Send up to 3 BG"
+        ],
+    },
+    {
+        name: "Professional",
+        pricing: {
+            monthly: 24.99,
+            annually: 249.99,
+        },
+        features: [
+            "1 TB Storage",
+            "5 Users Allowed",
+            "Send up to 10 BG"
+        ],
+    },
+    {
+        name: "Master",
+        pricing: {
+            monthly: 39.99,
+            annually: 399.99,
+        },
+        features: [
+            "2 TB Storage",
+            "10 Users Allowed",
+            "Send up to 20 BG"
+        ],
+    },
+]
